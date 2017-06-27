6590a345be109d0eebc0ecbf36f033cc2fc7b925
alexch <alex.chukanov@gmail.com>
2017-06-27 18:05:02 +0300

    1st ver

---
+++ JPMtest/JPMtest/Model.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPMtest
+{
+    class Model
+    {
+    }
+
+    enum ProductType
+    {
+        None,
+        Apple,
+        Orange,
+        Melon,
+        Pie,
+        Bananas
+    }
+
+    class Sale
+    {
+        int Id { get; set; }
+        public ProductType Product { get; set;}
+        public int Amount { get; set; }
+
+        int price = 0;
+        public int Price
+        {
+            get { return price; }
+            set { if (value < 0) price = 0; }
+        }
+
+        public ActionType Action { get; set; }
+        public int Adjustment { get; set; }
+        public MessageType SaleMessageType { get; set; }
+
+        public bool SaleAdjustment(Sale saleAdj)
+        {
+            bool result = true;
+
+            if (saleAdj.SaleMessageType == MessageType.Mes3)
+            {
+                switch(saleAdj.Action)
+                {
+                    case ActionType.Add:
+                        Price += saleAdj.Adjustment;
+                        //WriteLog();
+                        break;
+                    case ActionType.Subtruct:
+                        Price -= saleAdj.Adjustment;
+                        //if (Cost < 0)  WriteLog();
+                            break;
+                    case ActionType.Multiply:
+                        Price *= saleAdj.Adjustment;
+                        //WriteLog();
+                        break;
+                    default:
+                        result = false;
+                        //WriteLog();
+                        break;
+                }
+                
+            }
+            else
+            {
+                //WriteLog();
+                result = false;
+            }
+
+
+            return result;
+        }
+    }
+
+    enum ActionType
+    {
+        None,
+        Add,
+        Subtruct,
+        Multiply
+    }
+
+    enum MessageType
+    {
+        None,
+        Mes1,
+        Mes2,
+        Mes3
+    }
+
+}
---
+++ JPMtest/JPMtest/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPMtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Message format: <1>,<2>,<3>,<4>,<5>");
+            Console.WriteLine("<1> - Message Type: 1,2,3");
+            Console.WriteLine("<2> - Product: 1-apple,2-orange,3-pear");
+            Console.WriteLine("<3> - Cost, pence: ");
+            Console.WriteLine("<4> - Amount, items: ");
+            Console.WriteLine("<5> - Amendment, +/- pence: ");
+            Console.WriteLine();
+
+
+           Console.WriteLine("Enter E to exit");
+
+            string line;
+            bool result = true;
+
+            List<Sale> storedSales = new List<Sale>();
+
+            while ((line = Console.ReadLine()) != "E")
+            {
+                Sale sale = ParseMessage(line);                
+
+                switch (sale.SaleMessageType)
+                {
+                    case MessageType.Mes1:
+                        storedSales.Add(sale);
+                        break;
+                    case MessageType.Mes2:
+                        //AddSaleDetails(sale); 
+                        break;
+                    case MessageType.Mes3:
+                        //UpdateSaleDetails(sale); 
+                        break;
+                    default:
+                        result = false;
+                        break;
+                }
+
+                Console.WriteLine(result);
+                Console.WriteLine("bla bla - enter E to exit");
+            }
+
+        }
+
+        static Sale ParseMessage(string line)
+        {
+            Sale sale = null;
+
+            string[] words = line.Split(',' , ' ');
+
+            if(words.Count() == 5)
+            {
+                sale = new Sale();
+
+                int mt; 
+                int.TryParse(words[0], out mt);                
+                sale.SaleMessageType = (MessageType)mt;
+
+                int product;
+                int.TryParse(words[1], out product);
+                sale.Product = (ProductType)mt;
+
+                int price;
+                int.TryParse(words[2], out price);
+                sale.Price = price;
+
+                int amount;
+                int.TryParse(words[3], out amount);
+                sale.Amount = amount;
+
+                int adjustment;
+                int.TryParse(words[4], out adjustment);
+                sale.Adjustment = adjustment;
+                
+            }
+            else
+            {
+                //log
+            }
+
+            return sale;
+        }
+
+        static bool AddSaleDetails(Sale sale)
+        {
+            return false;
+        }
+
+        static bool UpdateSaleDetails(Sale sale)
+        {
+            return false;
+        }
+
+    }
+}
